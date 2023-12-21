8f0f0511cb188b62977400b35af0e4aee9c32d3b
Robbie-Kim <148425731+Robbie-Kim@users.noreply.github.com>
2023-12-21 17:49:21 +0900

    test

---
+++ src/main/java/branchtest/BranchTestClass.java
@@ -0,0 +1,4 @@
+package branchtest;
+
+public class BranchTestClass {
+}
---
+++ src/test/java/my/test/projectforgittest/ProjectForGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package my.test.projectforgittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectForGitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
